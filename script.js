50fe6367757360931786185ea3ee8a908fd7cddc
Aanya <wisegirl139@gmail.com>
2026-08-27 00:50:13 +0100

    okay lovee how a little bit goes a long way so made it fit device sixe and how many columns there are and also THE spreaddd. very mathsy, i like mathsy

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const canvas = document.getElementById("hack");
-const ctx=canvas.getContext("2d")
+const ctx=canvas.getContext("2d");
 let width;
 let height;
-const binary =[]
-const fontSize=16
+const binary =[];
+const fontSize=16;
+
+function resizeCanvas(){
+    width=canvas.width=window.innerWidth;
+    height=canvas.height=window.innerHeight;
+    binary.length=0;
+    const columns=Math.floor(width/fontSize);
+    for(let i=0;i,columns;i++){
+        binary.push({
+            x:i*fontSize,
+            y:Math.random()*height,
+            speed:1+Math.random()*2,
+            length:5+Math.floor(Math.random()*15)
+        });
+    }
+}
