0f4c13642df0911c1682661c0389f18d96191827
Aanya <wisegirl139@gmail.com>
2026-08-27 03:42:46 +0100

    sleepy time defo smth wront but thats not for today

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let height;
 const binary =[];
 const fontSize=20;
 
+
 function resizeCanvas(){
     width=canvas.width=window.innerWidth;
     height=canvas.height=window.innerHeight;
@@ -14,15 +15,11 @@ function resizeCanvas(){
         binary.push({
             x:i*fontSize,
             ud:Math.random()*height,
-            speed:1+Math.random()*3,
+            speed:2+Math.random()*3,
             length:5+Math.floor(Math.random()*15)
         });
     }
 }
-
-window.addEventListener("resize",resizeCanvas);
-resizeCanvas();
-
 function draw(){
     ctx.clearRect(0,0,width,height);
     ctx.font=`${fontSize}px "Courier New",monospace`;
@@ -38,10 +35,14 @@ function draw(){
         }
         stream.ud+=stream.speed;
         if(stream.ud>height+stream.length*fontSize.height){
+            console.log("reset")
             stream.ud=-stream.length*fontSize;
         }
     }
     requestAnimationFrame(draw);
 }
+window.addEventListener("resize",resizeCanvas);
+resizeCanvas();
+
 draw();
 
