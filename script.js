50d38beb094347c6fdbeeeb8eb3861900cdaeceb
Aanya <wisegirl139@gmail.com>
2026-08-27 01:09:05 +0100

    dont look off into space when u type or you forget to press shift kinda errors

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function resizeCanvas(){
     height=canvas.height=window.innerHeight;
     binary.length=0;
     const columns=Math.floor(width/fontSize);
-    for(let i=0;i,columns;i++){
+    for(let i=0;i<columns;i++){
         binary.push({
             x:i*fontSize,
-            y:Math.random()*height,
+            ud:Math.random()*height,
             speed:1+Math.random()*2,
             length:5+Math.floor(Math.random()*15)
         });
@@ -25,9 +25,9 @@ resizeCanvas();
 
 function draw(){
     ctx.clearRect(0,0,width,height);
-    ctx.font='${fontSize}px "Courier New",monospace';
+    ctx.font=`${fontSize}px "Courier New",monospace`;
     for (let stream of binary){
-        for(let i=0; i,stream.length;i++){
+        for(let i=0; i<stream.length;i++){
             const ud=stream.ud-i*fontSize;
             ctx.fillText(
                 Math.random()>0.5?"0":"1",
