d4f5457422b1c6a3b35c4d6d04754a249cdaf5e0
Aanya <wisegirl139@gmail.com>
2026-08-28 01:37:42 +0100

    added a vertical offset to the stars gonna spread out more tho bc they still kinda tiny

--- aanyaweb/aanya.js
+++ aanyaweb/aanya.js
@@ -5,7 +5,7 @@ for(let i = 0;i<4;i++){
     star.src=starim;
     star.classList.add("star");
     star.style.left="-50px";
-    star.style.top="25%";
+    star.style.top=(20+i*4)+"%";
     star.style.animationDuration=(3+Math.random()*2)+"s";
     document.body.appendChild(star);
 }
