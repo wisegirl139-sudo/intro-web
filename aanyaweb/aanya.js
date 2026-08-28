319ad679eaec05aa9449e839ebafdfffe4a36351
Aanya <wisegirl139@gmail.com>
2026-08-28 01:55:11 +0100

    looks peak , im sooooo done im finishedd how did this take like 8 hrs anywyas i also linked it to main html but yee

--- aanyaweb/aanya.js
+++ aanyaweb/aanya.js
@@ -1,11 +1,12 @@
 const starcont=document.body;
 const starim= "img/star.png"
-for(let i = 0;i<4;i++){
-    const star=document.createElement("img");
-    star.src=starim;
+
+for(let i = 0;i<8;i++){
+    const star=document.createElement("div");
     star.classList.add("star");
+    star.innerHTML=`<img src="${starim}">`;
     star.style.left="-50px";
-    star.style.top=(20+i*4)+"%";
+    star.style.top=(10+i*9)+"%";
     star.style.animationDuration=(3+Math.random()*2)+"s";
     document.body.appendChild(star);
 }
