0c2a3b190f329fcde8aac51041e8090af2104ed2
Aanya <wisegirl139@gmail.com>
2026-08-28 01:30:35 +0100

    js doneish , i cant really tell im testing out doing it css so well see when i do the main bit

--- aanyaweb/aanya.js
+++ aanyaweb/aanya.js
@@ -1,2 +1,11 @@
 const starcont=document.body;
-const starim= "img/"
+const starim= "img/star.png"
+for(let i = 0;i<4;i++){
+    const star=document.createElement("img");
+    star.src=starim;
+    star.classList.add("star");
+    star.style.left="-50px";
+    star.style.top="25%";
+    star.style.animationDuration=(3+Math.random()*2)+"s";
+    document.body.appendChild(star);
+}
